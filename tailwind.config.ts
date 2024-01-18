1b8b0d744623c89c84da92f8cd3d1dcf5acb0f4b
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2024-01-18 17:38:26 +0700

    update tailwind content path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,9 @@ import { withUt } from 'uploadthing/tw';
 const config = withUt({
   darkMode: ['class'],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}',
   ],
   theme: {
     container: {
